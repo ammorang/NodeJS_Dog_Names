7037557169bd35e31be6f024bdc921aafec5009f
amanda <am.morang@gmail.com>
2018-06-04 14:59:40 -0400

    first commit of nodejs lab

---
+++ dognames.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const dogs = ["Robert 'Bobby' the Bulldog", "Patrick the Poodle", "Cheryl the Chihuahua", "Ronald the Rottweiler", "Harvey the Huskey", "Greg the Greyhound", "Theresa the Terrier", "Millie the Mastiff", "Leonard the Laborador", "Sylvia the Sheepdog"];
+
+module.exports = dogs;
---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const http = require("http");
+const dogs = require("./dognames");
+
+
+
+http.createServer((request, response) => {
+  response.writeHead(200, {
+    "Content-type": "text/plain"
+  });
+  response.write(`You've just made a new friend! Meet ${dogs[Math.floor(Math.random()*dogs.length)]}!`);
+  response.end();
+}).listen(3000);
+
